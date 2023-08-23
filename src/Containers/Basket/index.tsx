d147ff887667de5e35e97e55016b5ffaeff9ac88
stygianlgdonic <stygianlgdonic@gmail.com>
2023-08-23 23:12:00 +0500

    finalized

--- src/Containers/Basket/index.tsx
+++ src/Containers/Basket/index.tsx
@@ -3,6 +3,7 @@ import { RootState } from "../../redux/store";
 import BasketItem from './BasketItem';
 import { Link } from 'react-router-dom';
 import backBtn from '../../assets/icons/backbtn.svg'
+import emptyCart from '../../assets/images/emptyCart.webp'
 const Basket = () => {
     const { items } = useSelector((state: RootState) => state.cart);
     const totalPrice = items.reduce((acc, item) => {
@@ -11,7 +12,7 @@ const Basket = () => {
     }, 0)
     return (
         <div className='p-4'>
-            <div className='flex justify-between w-1/2'>
+            <div className='flex justify-between'>
                 <Link to={"/"} className="flex hover:bg-gray-300 h-14 w-14 rounded-xl justify-center">
                     <img src={backBtn} alt="Back" width={30} height={30} />
                 </Link>
@@ -19,15 +20,19 @@ const Basket = () => {
                     <p className="text-4xl font-bold mb-6 text-center text-green-600">Shopping Cart</p>
                     <p className="text-xl text-green-600">{`(${items.length})`}</p>
                 </div>
+                <div />
             </div>
-            {
+            {items.length > 0 ?
                 items.map(item => (
                     <BasketItem
                         key={item.id}
                         product={item}
                     />)
+                ) : (
+                    <div className='flex justify-center'>
+                        <img src={emptyCart} />
+                    </div>
                 )
-
             }
             <p className="text-2xl font-bold mb-6 text-right">{`Total: $${totalPrice.toFixed(2)}`}</p>
         </div>
