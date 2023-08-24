fa11d43a0601cbe7052c09fa8a3c72fa139f8671
stygianlgdonic <stygianlgdonic@gmail.com>
2023-08-24 21:19:01 +0500

    pretty

--- src/components/ProductItem/index.test.tsx
+++ src/components/ProductItem/index.test.tsx
@@ -1,50 +1,55 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { fireEvent, render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import ProductItem from '.';
-import cartReducer from '../../redux/cartSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { fireEvent, render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import ProductItem from ".";
+import cartReducer from "../../redux/cartSlice";
 
-describe('ProductItem Component', () => {
-    const mockProduct = {
-        id: 1,
-        img: 'path_to_image.jpg',
-        name: 'Sample Product',
-        price: 100,
-        colour: 'blue'
-    };
-    
-    const store = configureStore({
-        reducer: {
-            cart: cartReducer
-        }
-    });
+describe("ProductItem Component", () => {
+  const mockProduct = {
+    id: 1,
+    img: "path_to_image.jpg",
+    name: "Sample Product",
+    price: 100,
+    colour: "blue",
+  };
 
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  const store = configureStore({
+    reducer: {
+      cart: cartReducer,
+    },
+  });
 
-    it('renders correctly', () => {
-        const { getByText, getByAltText } = render(
-            <Provider store={store}>
-                <ProductItem product={mockProduct} />
-            </Provider>
-        );
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-        expect(getByText('Sample Product')).toBeInTheDocument();
-        expect(getByAltText('Sample Product')).toHaveAttribute('src', 'path_to_image.jpg');
-        expect(getByText('Price: $100.00')).toBeInTheDocument();
-    });
+  it("renders correctly", () => {
+    const { getByText, getByAltText } = render(
+      <Provider store={store}>
+        <ProductItem product={mockProduct} />
+      </Provider>,
+    );
 
-    it('dispatches addToCart action when "Add to cart" button is clicked', () => {
-        const { getByRole } = render(
-            <Provider store={store}>
-                <ProductItem product={mockProduct} />
-            </Provider>
-        );
+    expect(getByText("Sample Product")).toBeInTheDocument();
+    expect(getByAltText("Sample Product")).toHaveAttribute(
+      "src",
+      "path_to_image.jpg",
+    );
+    expect(getByText("Price: $100.00")).toBeInTheDocument();
+  });
 
-        const addToCartButton = getByRole('button', { name: /Add to cart/i });
-        fireEvent.click(addToCartButton);
+  it('dispatches addToCart action when "Add to cart" button is clicked', () => {
+    const { getByRole } = render(
+      <Provider store={store}>
+        <ProductItem product={mockProduct} />
+      </Provider>,
+    );
 
-        expect(store.getState().cart.items).toEqual([{ ...mockProduct, quantity: 1 }]);
-    });
+    const addToCartButton = getByRole("button", { name: /Add to cart/i });
+    fireEvent.click(addToCartButton);
+
+    expect(store.getState().cart.items).toEqual([
+      { ...mockProduct, quantity: 1 },
+    ]);
+  });
 });
--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -1,59 +1,57 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { addToCart, updateCart } from "../../redux/cartSlice";
 import { RootState } from "../../redux/store";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
-const ProductItem = ({
-    product,
-}: ProductItemProps) => {
-    const dispatch = useDispatch();
-    const { id, img, name, price } = product;
-    const { items } = useSelector((state: RootState) => state.cart);
+const ProductItem = ({ product }: ProductItemProps) => {
+  const dispatch = useDispatch();
+  const { id, img, name, price } = product;
+  const { items } = useSelector((state: RootState) => state.cart);
 
-    const handleAddToCart = () => {
-        const existingItem = items.find((item: Product) => item.id === id);
-        const eiq = existingItem?.quantity || 0;
-        if (existingItem) {
-            const updatedItems = items.map(item => {
-                if (item.id === id) {
-                    return { ...product, quantity: eiq + 1 }
-                } else {
-                    return item;
-                }
-            })
-            dispatch(updateCart(updatedItems));
+  const handleAddToCart = () => {
+    const existingItem = items.find((item: Product) => item.id === id);
+    const eiq = existingItem?.quantity || 0;
+    if (existingItem) {
+      const updatedItems = items.map((item) => {
+        if (item.id === id) {
+          return { ...product, quantity: eiq + 1 };
         } else {
-            dispatch(addToCart({ ...product, quantity: 1 }));
+          return item;
         }
-        toast.success('Item added!', {
-            position: "top-right",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "colored",
-        });
-    };
+      });
+      dispatch(updateCart(updatedItems));
+    } else {
+      dispatch(addToCart({ ...product, quantity: 1 }));
+    }
+    toast.success("Item added!", {
+      position: "top-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
+  };
 
-    return (
-        <div className="border p-4 flex items-center justify-between mb-4 rounded">
-            <div className="flex items-center">
-                <img src={img} alt={name} className="w-16 h-24 mr-4 rounded" />
-                <div className="flex flex-col gap-y-5">
-                    <h3 className="text-lg font-semibold">{name}</h3>
-                    <p>{`Price: $${price.toFixed(2)}`}</p>
-                </div>
-            </div>
-            <button
-                onClick={handleAddToCart}
-                className="bg-green-100 text-green-600 px-3 py-1 rounded-md hover:bg-green-200 transition-colors"
-            >
-                Add to cart
-            </button>
+  return (
+    <div className="border p-4 flex items-center justify-between mb-4 rounded">
+      <div className="flex items-center">
+        <img src={img} alt={name} className="w-16 h-24 mr-4 rounded" />
+        <div className="flex flex-col gap-y-5">
+          <h3 className="text-lg font-semibold">{name}</h3>
+          <p>{`Price: $${price.toFixed(2)}`}</p>
         </div>
-    );
+      </div>
+      <button
+        onClick={handleAddToCart}
+        className="bg-green-100 text-green-600 px-3 py-1 rounded-md hover:bg-green-200 transition-colors"
+      >
+        Add to cart
+      </button>
+    </div>
+  );
 };
 
 export default ProductItem;
